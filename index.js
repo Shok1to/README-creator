3f7c8e8fc7f1c9091b41f038555516a5938ef8d5
Shok1to <shoko.takahashi.911@gmail.com>
2023-05-17 00:52:16 -0400

    install inquirer

---
+++ index.js
@@ -0,0 +1,44 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type:'input',
+        message: 'What is the title of your project?',
+        name:'title',
+    },
+    {
+        type:'input'
+        message:'Please provide the description of your project.'
+        name:'description'
+    },
+    {
+        type:'input'
+        message:''
+        name:''
+    },
+    {
+        type:'input'
+        message:''
+        name:''
+    },
+    {
+        type:'input'
+        message:''
+        name:''
+    },
+    {
+        type:'input'
+        message:''
+        name:''
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
