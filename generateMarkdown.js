f8a081cd04e3621e8e917381a5fa2e2b9a0a751d
Shok1to <shoko.takahashi.911@gmail.com>
2023-05-17 22:55:11 -0400

    added questions

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![${license}](https://img.shields.io/badge/license-${license}-lightgrey.png)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +15,40 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Test](#test)
+* [Questions](#questions)
+
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contribution}
+
+## Test
+
+${data.test}
+
+## Questions
+
+If you have any further questions, please email me at [${data.email}](mailto:${data.email}) or reach me on Github at [${data.github}](https://github.com/${data.github}).
+
 
 `;
 }
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const path = require('path')
+const generateMarkdown = require('./generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -8,37 +12,63 @@ const questions = [
         name:'title',
     },
     {
-        type:'input'
-        message:'Please provide the description of your project.'
-        name:'description'
+        type:'input',
+        message:'Please provide the description of your project.',
+        name:'description',
     },
     {
-        type:'input'
-        message:''
-        name:''
+        type:'input',
+        message:'What is your installation instructions ?',
+        name:'install',
     },
     {
-        type:'input'
-        message:''
-        name:''
+        type:'input',
+        message:'What is your usage ?',
+        name:'usage',
     },
     {
-        type:'input'
-        message:''
-        name:''
+        type:'input',
+        message:'What is your contribution guidelines?',
+        name:'contribution',
     },
     {
-        type:'input'
-        message:''
-        name:''
+        type:'input',
+        message:'What is your test instructions ?',
+        name:'test',
     },
+    {
+        type:'input',
+        message:'What is your github name ?',
+        name:'github',
+    },
+    {
+        type:'input',
+        message:'What is your email ?',
+        name:'email',
+    },
+    {
+        type:'list',
+        message:'What license would you like to use ?',
+        name:'license',
+        choices: ['Apache','GNU','MIT','BSD'],
+    },
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(answers) {
+        console.log(answers)
+
+        fs.writeFileSync('./userREADME.md', generateMarkdown({...answers}))
+    })
+
+
+}
 
 // Function call to initialize app
 init();
